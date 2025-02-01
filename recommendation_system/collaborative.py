9ec28d7d11751c6a020a38ee1a3aa811a9846795
Julian Sotelo <103071349+F00LIAN@users.noreply.github.com>
2025-02-01 11:52:52 -0800

    Update collaborative.py

--- recommendation_system/collaborative.py
+++ recommendation_system/collaborative.py
@@ -1,23 +1,5 @@
-import pandas as pd
 import numpy as np
-from sklearn.metrics.pairwise import linear_kernel
-from sklearn.feature_extraction.text import CountVectorizer 
-from sklearn.feature_extraction.text import TfidfVectorizer  # TF-IDF
-from sklearn.preprocessing import scale
-from sklearn.cluster import KMeans
-from sklearn.decomposition import TruncatedSVD
-from sklearn.decomposition import NMF
 from sklearn.metrics.pairwise import cosine_similarity
-import nltk 
-from nltk.corpus import stopwords 
-from nltk.stem import LancasterStemmer, PorterStemmer
-from nltk.stem import WordNetLemmatizer
-nltk.download('vader_lexicon')
-#import spacy #for faster tokenization and lemmatization
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
-import re
-import string
 from utils.helpers import preprocess_data
 
 
@@ -44,6 +26,9 @@ def recommend(recipe_name, pt, similarity_scores):
 # Main function to run the recommendation
 def run_collaborative(recipe_name):
     pt = preprocess_data()
+    similarity_scores = calculate_similarity(pt)
+    recommendations = recommend(recipe_name, pt, similarity_scores)
+    return recommendations
     similarity_scores = calculate_similarity(pt)
     recommendations = recommend(recipe_name, pt, similarity_scores)
     return recommendations
